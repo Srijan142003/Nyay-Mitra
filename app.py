cb3f8c8ec94c8a4013a495e07d603e55eb769d30
Srijan Kundu <108989337+Srijan142003@users.noreply.github.com>
2025-11-10 14:13:45 +0530

    Configure app to use environment port variable

--- app.py
+++ app.py
@@ -540,4 +540,6 @@ def admin_dashboard():
     return render_template('admin.html', stats=stats)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+        import os
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
